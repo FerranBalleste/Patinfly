3acb4ea363957a4351559a44e20558570f17ee65
FerranBalleste <fe.ba.solsona@gmail.com>
2022-04-02 20:28:55 +0200

    Now it's able to change theme from light to dark in Settings.

--- app/src/main/java/com/example/patinfly/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/patinfly/fragments/SettingsFragment.kt
@@ -1,12 +1,30 @@
 package com.example.patinfly.fragments
 
+import android.content.SharedPreferences
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
+import androidx.preference.SwitchPreferenceCompat
 import com.example.patinfly.R
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
+
+        val prefs = PreferenceManager.getDefaultSharedPreferences(activity)
+        val listener = SharedPreferences.OnSharedPreferenceChangeListener { prefs, key ->
+            if (key == "theme") {
+                val theme = prefs.getString("theme", "Light")
+                if(theme == "Light")
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                else
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }
+        }
+        prefs.registerOnSharedPreferenceChangeListener(listener)
     }
+
 }
